c975d95bed3f69423f2e9dd4f790e08f99275c66
William Ferguson <william.t.ferguson.jr@gmail.com>
2025-11-02 17:29:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "pawsitive-image",
+  "appId": "1:615130299562:web:a3231ede92dcf6716064d3",
+  "apiKey": "AIzaSyBpaQk3CGo80lv4kUdzmc7GA6m3oDmy8uM",
+  "authDomain": "pawsitive-image.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "615130299562"
+};
