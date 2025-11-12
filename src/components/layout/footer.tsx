3a3baa5b9db471491fc8f02ae8b923579a75cb4a
William Ferguson <william.t.ferguson.jr@gmail.com>
2025-11-12 01:00:45 +0000

    change color to lilac for this icon (_for element svg_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,3 +1,4 @@
+
 import { PawPrint, Facebook, Instagram, Mail } from 'lucide-react';
 import Link from 'next/link';
 
@@ -6,7 +7,7 @@ export function Footer() {
     <footer className="border-t border-border/40 bg-background">
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <PawPrint className="h-6 w-6 text-primary hidden sm:block" />
+          <PawPrint className="h-6 w-6 text-accent hidden sm:block" />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
             © {new Date().getFullYear()} Suds n' Wiggles. All Rights Reserved.
           </p>
